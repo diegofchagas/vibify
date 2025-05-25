13a5f406eb138df5caffadfa9bb48e73d724eb27
Diego Chagas <diegofchagas@gmail.com>
2025-05-25 13:31:31 -0300

    error report

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,11 @@
+
+import logo from "../../assets/spotify-logo.png";
+
+export const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="" />
+      <h1>Vibify</h1>
+    </header>
+  );
+};
