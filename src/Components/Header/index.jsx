727c54622fc93b043e6bac0eb30159961adb3fdb
Diego Chagas <diegofchagas@gmail.com>
2025-05-26 12:06:43 -0300

    subindo header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,12 +1,12 @@
 
 import logo from "../../assets/spotify-logo.png";
+import { ConateinerHeader } from "./style";
 
 export const Header = () => {
   return (
-    <header>
+    <ConateinerHeader>
       <img src={logo} alt="" />
       <a href="#">Vibify</a>
-    
-    </header>
+    </ConateinerHeader>
   );
 };
--- src/Components/Header/style.jsx
+++ src/Components/Header/style.jsx
@@ -1,1 +1,21 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+export const ConateinerHeader = styled.header`
+  background-color:black;
+  display:flex;
+  align-items:center;
+  justify-content:space-between;
+  padding: .800rem;
+
+  a{
+    text-decoration:none;
+    font-weight:bold;
+    font-size:1.5rem;
+    color:white;
+  }
+
+  a:hover{
+    text-decoration:underline;
+  }
+`;
+
