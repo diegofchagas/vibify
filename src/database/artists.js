12f6b73483485c87f5773972ec6f690d1aa0a767
Diego Chagas <diegofchagas@gmail.com>
2025-06-03 11:25:09 -0300

    add arrays de dados temporarios para funcionamento do projeto e testes

---
+++ src/database/artists.js
@@ -0,0 +1,270 @@
+// export const artistArray = [
+//   {
+//     id: 1,
+  
+//     name: "Henrique & Juliano",
+//     banner: "https://i.scdn.co/image/ab67618600001016b37fbcbd078cb239588df5d9",
+//     song: "Última Saudade - Ao Vivo"
+//   },
+//   {
+//     id: 2,
+//     image: "https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9",
+//     name: "MC Tuto",
+//     banner:
+//       "https://www.hashtagtreinamentos.com/wp-content/uploads/2025/02/mc-tuto.png",
+//   },
+//   {
+//     id: 3,
+//     image: "https://i.scdn.co/image/ab6761610000517477937baabc49dea13c17c706",
+//     name: "Jorge & Mateus",
+//     banner: "https://i.scdn.co/image/ab67618600001016e2952bec9cfd4e6862e23607",
+//   },
+//   {
+//     id: 4,
+//     image: "https://i.scdn.co/image/ab67616100005174b4c07f06044ca6f1a8548192",
+//     name: "NATTAN",
+//     banner: "https://i.scdn.co/image/ab67618600001016fc9f6be028f419e9e47c646f",
+//   },
+//   {
+//     id: 5,
+//     image: "https://i.scdn.co/image/ab676161000051747ffc3038ef5e67354ecdede1",
+//     name: "Grupo Menos É Mais",
+//     banner: "https://i.scdn.co/image/ab67618600001016e06c8846b7f03fdb987739fb",
+//   },
+//   {
+//     id: 6,
+//     image: "https://i.scdn.co/image/ab676161000051747098ffe23a919f7742979c6e",
+//     name: "Zé Neto & Cristiano",
+//     banner: "https://i.scdn.co/image/ab6761860000101695c836c767102509e662cb63",
+//   },
+//   {
+//     id: 7,
+//     image: "https://i.scdn.co/image/ab67616100005174b97627e4ea832ed67617627a",
+//     name: "Matheus & Kauan",
+//     banner: "https://i.scdn.co/image/ab676186000010162c57b647c5d5994151c5ef4d",
+//   },
+//   {
+//     id: 8,
+//     image: "https://i.scdn.co/image/ab67616100005174668a5535093f4cc53b1fef45",
+//     name: "Oruam",
+//     banner:
+//       "https://www.hashtagtreinamentos.com/wp-content/uploads/2025/02/oruam.png",
+//   },
+//   {
+//     id: 9,
+//     image: "https://i.scdn.co/image/ab67616100005174acfec0b96ba96562ad2e7fa3",
+//     name: "Murilo Huff",
+//     banner: "https://i.scdn.co/image/ab67618600001016909a162796dbc3629a1fadeb",
+//   },
+//   {
+//     id: 10,
+//     image: "https://i.scdn.co/image/ab6761610000517483f19000f18fbec5cf328f59",
+//     name: "Hugo & Guilherme",
+//     banner: "https://i.scdn.co/image/ab67618600001016e530cc641fb0c98881d25c7b",
+//   },
+//   {
+//     id: 11,
+//     image: "https://i.scdn.co/image/ab67616100005174ec1fef19ee9676f9ce662f39",
+//     name: "MC LUUKY",
+//     banner: "https://i.scdn.co/image/ab676186000010161db163263f4389becf8f10b2",
+//   },
+//   {
+//     id: 12,
+//     image: "https://i.scdn.co/image/ab67616100005174281fcf09827f1223ca4d729c",
+//     name: "Léo Foguete",
+//     banner: "https://i.scdn.co/image/ab6761860000101626675729154154577b58cf48",
+//   },
+//   {
+//     id: 13,
+//     image: "https://i.scdn.co/image/ab67616100005174dbfd9763ee6948c110999581",
+//     name: "Gusttavo Lima",
+//     banner: "https://i.scdn.co/image/ab676186000010168c28ce6bfae4955fc9e07a4f",
+//   },
+//   {
+//     id: 14,
+//     image: "https://i.scdn.co/image/ab676161000051744ba71e7bb7139281802eb42e",
+//     name: "Nilo",
+//     banner: "https://i.scdn.co/image/ab67618600001016fbb9f8fe09bac68f3b7ad0d0",
+//   },
+//   {
+//     id: 15,
+//     image: "https://i.scdn.co/image/ab67616100005174121da419911d33812cfd481e",
+//     name: "Luan Pereira",
+//     banner: "https://i.scdn.co/image/ab67618600001016293cffc5c4ade8d601fc9042",
+//   },
+//   {
+//     id: 16,
+//     image: "https://i.scdn.co/image/ab67616100005174946d345050cf85e1b67e00d3",
+//     name: "Guilherme & Benuto",
+//     banner: "https://i.scdn.co/image/ab676186000010167e05defd0907af8848945096",
+//   },
+//   {
+//     id: 17,
+//     image: "https://i.scdn.co/image/ab67616100005174ffaebdbc972967729f688e25",
+//     name: "Marília Mendonça",
+//     banner: "https://i.scdn.co/image/ab676186000010169e292b87a6e3a0900e871692",
+//   },
+//   {
+//     id: 18,
+//     image: "https://i.scdn.co/image/ab67616100005174fde13b4ff04ee1c0f33c6878",
+//     name: "Luan Santana",
+//     banner: "https://i.scdn.co/image/ab676186000010168b1f0064bf4dcb45cbae94e0",
+//   },
+//   {
+//     id: 19,
+//     image: "https://i.scdn.co/image/ab67616100005174a291b99de1602566f8d57725",
+//     name: "Gustavo Mioto",
+//     banner: "https://i.scdn.co/image/ab67618600001016404a5d03d5a37547177c0f2a",
+//   },
+//   {
+//     id: 20,
+//     image: "https://i.scdn.co/image/ab67616100005174a4d221889e774d4b173aeaa0",
+//     name: "Henry Freitas",
+//     banner: "https://i.scdn.co/image/ab676186000010169415572375bac1960f9de7e5",
+//   },
+// ];
+
+
+export const artistArray = [
+  {
+    id: 1,
+    image: "https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4",
+    name: "Henrique & Juliano",
+    banner: "https://i.scdn.co/image/ab67618600001016b37fbcbd078cb239588df5d9",
+    song: "Última Saudade - Ao Vivo", // Já estava correto
+    
+  },
+  {
+    id: 2,
+    image: "https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9",
+    name: "MC Tuto",
+    banner: "https://www.hashtagtreinamentos.com/wp-content/uploads/2025/02/mc-tuto.png",
+    song: "Pode Falar Que Não Me Ama" // Exemplo de música do MC Tuto
+  },
+  {
+    id: 3,
+    image: "https://i.scdn.co/image/ab6761610000517477937baabc49dea13c17c706",
+    name: "Jorge & Mateus",
+    banner: "https://i.scdn.co/image/ab67618600001016e2952bec9cfd4e6862e23607",
+    song: "Tijolão" // Música de Jorge & Mateus
+  },
+  {
+    id: 4,
+    image: "https://i.scdn.co/image/ab67616100005174b4c07f06044ca6f1a8548192",
+    name: "NATTAN",
+    banner: "https://i.scdn.co/image/ab67618600001016fc9f6be028f419e9e47c646f",
+    song: "Comunicação" // Música do NATTAN
+  },
+  {
+    id: 5,
+    image: "https://i.scdn.co/image/ab676161000051747ffc3038ef5e67354ecdede1",
+    name: "Grupo Menos É Mais",
+    banner: "https://i.scdn.co/image/ab67618600001016e06c8846b7f03fdb987739fb",
+    song: "Lapada Dela (Ao Vivo)" // Música do Grupo Menos É Mais
+  },
+  {
+    id: 6,
+    image: "https://i.scdn.co/image/ab676161000051747098ffe23a919f7742979c6e",
+    name: "Zé Neto & Cristiano",
+    banner: "https://i.scdn.co/image/ab6761860000101695c836c767102509e662cb63",
+    song: "Vazou na Braquiara" // Música de Zé Neto & Cristiano
+  },
+  {
+    id: 7,
+    image: "https://i.scdn.co/image/ab67616100005174b97627e4ea832ed67617627a",
+    name: "Matheus & Kauan",
+    banner: "https://i.scdn.co/image/ab676186000010162c57b647c5d5994151c5ef4d",
+    song: "Pense Em Mim (Ao Vivo)" // Música de Matheus & Kauan
+  },
+  {
+    id: 8,
+    image: "https://i.scdn.co/image/ab67616100005174668a5535093f4cc53b1fef45",
+    name: "Oruam",
+    banner: "https://www.hashtagtreinamentos.com/wp-content/uploads/2025/02/oruam.png",
+    song: "Deus Me Livre" // Música do Oruam
+  },
+  {
+    id: 9,
+    image: "https://i.scdn.co/image/ab67616100005174acfec0b96ba96562ad2e7fa3",
+    name: "Murilo Huff",
+    banner: "https://i.scdn.co/image/ab67618600001016909a162796dbc3629a1fadeb",
+    song: "Anestesiado" // Música do Murilo Huff
+  },
+  {
+    id: 10,
+    image: "https://i.scdn.co/image/ab6761610000517483f19000f18fbec5cf328f59",
+    name: "Hugo & Guilherme",
+    banner: "https://i.scdn.co/image/ab67618600001016e530cc641fb0c98881d25c7b",
+    song: "Mal Feito (feat. Marília Mendonça)" // Música de Hugo & Guilherme
+  },
+  {
+    id: 11,
+    image: "https://i.scdn.co/image/ab67616100005174ec1fef19ee9676f9ce662f39",
+    name: "MC LUUKY",
+    banner: "https://i.scdn.co/image/ab676186000010161db163263f4389becf8f10b2",
+    song: "O Que Aconteceu" // Música do MC LUUKY
+  },
+  {
+    id: 12,
+    image: "https://i.scdn.co/image/ab67616100005174281fcf09827f1223ca4d729c",
+    name: "Léo Foguete",
+    banner: "https://i.scdn.co/image/ab6761860000101626675729154154577b58cf48",
+    song: "Novinha Dança (feat. Dj Arana)" // Música do Léo Foguete
+  },
+  {
+    id: 13,
+    image: "https://i.scdn.co/image/ab67616100005174dbfd9763ee6948c110999581",
+    name: "Gusttavo Lima",
+    banner: "https://i.scdn.co/image/ab676186000010168c28ce6bfae4955fc9e07a4f",
+    song: "Coração Cigano (Ao Vivo)" // Música do Gusttavo Lima
+  },
+  {
+    id: 14,
+    image: "https://i.scdn.co/image/ab676161000051744ba71e7bb7139281802eb42e",
+    name: "Nilo",
+    banner: "https://i.scdn.co/image/ab67618600001016fbb9f8fe09bac68f3b7ad0d0",
+    song: "Seu Brilho (feat. Mc Ryan Sp)" // Música do Nilo
+  },
+  {
+    id: 15,
+    image: "https://i.scdn.co/image/ab67616100005174121da419911d33812cfd481e",
+    name: "Luan Pereira",
+    banner: "https://i.scdn.co/image/ab67618600001016293cffc5c4ade8d601fc9042",
+    song: "Dentro da Hilux (Ao Vivo)" // Música do Luan Pereira
+  },
+  {
+    id: 16,
+    image: "https://i.scdn.co/image/ab67616100005174946d345050cf85e1b67e00d3",
+    name: "Guilherme & Benuto",
+    banner: "https://i.scdn.co/image/ab676186000010167e05defd0907af8848945096",
+    song: "3 Batidas" // Música de Guilherme & Benuto
+  },
+  {
+    id: 17,
+    image: "https://i.scdn.co/image/ab67616100005174ffaebdbc972967729f688e25",
+    name: "Marília Mendonça",
+    banner: "https://i.scdn.co/image/ab676186000010169e292b87a6e3a0900e871692",
+    song: "Leão" // Música de Marília Mendonça
+  },
+  {
+    id: 18,
+    image: "https://i.scdn.co/image/ab67616100005174fde13b4ff04ee1c0f33c6878",
+    name: "Luan Santana",
+    banner: "https://i.scdn.co/image/ab676186000010168b1f0064bf4dcb45cbae94e0",
+    song: "Morena" // Música do Luan Santana
+  },
+  {
+    id: 19,
+    image: "https://i.scdn.co/image/ab67616100005174a291b99de1602566f8d57725",
+    name: "Gustavo Mioto",
+    banner: "https://i.scdn.co/image/ab67618600001016404a5d03d5a37547177c0f2a",
+    song: "Com ou Sem Mim" // Música do Gustavo Mioto
+  },
+  {
+    id: 20,
+    image: "https://i.scdn.co/image/ab67616100005174a4d221889e774d4b173aeaa0",
+    name: "Henry Freitas",
+    banner: "https://i.scdn.co/image/ab676186000010169415572375bac1960f9de7e5",
+    song: "Tem Cafe (Ao Vivo)" // Música do Henry Freitas
+  },
+];
