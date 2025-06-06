b2ed9e3bfc66cb63c07258d0d45de0dab82a3688
Diego Chagas <diegofchagas@gmail.com>
2025-06-06 10:23:36 -0300

    includido nova variavel para indetificao de cada card via id e rota

--- src/Components/BandsSection/CardsArtists/index.jsx
+++ src/Components/BandsSection/CardsArtists/index.jsx
@@ -1,15 +1,19 @@
 import { PlayCircleIcon } from '@phosphor-icons/react'
 import { CardContainer, CardImage, TextBox } from './style'
+import { Link } from 'react-router'
 
-export const CardsArtists = ({id, name,image,banner,song}) => {
+export const CardsArtists = ({id, name,image,song, cardType}) => {
+
+  //const routePrefix = cardType === 'songs' ? "/song" : "/artist"
+
   return (
-    <CardContainer>
+      <CardContainer to={`${cardType}/${id}`}>
       <CardImage>
       <img src={image} alt={`imagem do artista ${name}` } />
       </CardImage>
       <TextBox className='box-texts'>
-      <strong> <a href="http://">{name}</a></strong>
-      <a>{song ?? "Artista"}</a>
+      <strong> <Link to={`${cardType}/${id}`}>{name}</Link></strong>
+      <span>{song ?? "Artista"}</span>
       </TextBox>
       <PlayCircleIcon size={48} weight='fill' />
     </CardContainer>
