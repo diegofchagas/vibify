06133e0ec03c48c3f10c87ccfac7329d2ac9dc23
Diego Chagas <diegofchagas@gmail.com>
2025-06-06 10:27:12 -0300

    add de hooks para controle de rotas e renderizacao de componentes via dados recebidos

--- src/Components/BandsSection/SectionList/index.jsx
+++ src/Components/BandsSection/SectionList/index.jsx
@@ -1,24 +1,36 @@
 import { ContainerArtists, ContainerList, SectionDiscography } from "./style";
 import { CardsArtists } from "../CardsArtists";
+import { Link, useLocation } from "react-router";
 
-export const SectionList = ({ title,data }) => {
-  if(!data || data.length === 0 ){
+export const SectionList = ({ title, data, linktoAll, cardType }) => {
+  const { pathname } = useLocation();
+  const isHome = pathname === "/";
+  const limitedData = isHome ? data.slice(0, 20) : data;
+  
+  if (!data) {
+    return <p>Carregando...</p>;
+  }
+
+  if (!data || data.length === 0) {
     return <p>nada encontrado</p>;
   }
 
   return (
     <SectionDiscography>
+      {/* criar nova pagina de musicas em alta para ajustar esse link do titulo */}
       <ContainerArtists>
         <a href="#" className="trending-music">
           {title}
         </a>
-        <a href="#" className="show-all">
-          Mostrar tudo
-        </a>
+        {isHome && (
+          <Link to={linktoAll} className="show-all">
+            Mostrar tudo
+          </Link>
+        )}
       </ContainerArtists>
       <ContainerList>
-        {data.map(( artist ) => (
-          <CardsArtists key={artist.id} {...artist} />
+        {limitedData .map((item) => (
+          <CardsArtists key={item.id} {...item} cardType={cardType} />
         ))}
       </ContainerList>
     </SectionDiscography>
