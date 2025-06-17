23b0e6f4372e6d77ae103c9153f26211ce9490ef
Diego Chagas <diegofchagas@gmail.com>
2025-06-17 11:05:08 -0300

    add novas paginas e rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
-import { BandsSection } from "./Components/BandsSection";
 import { Header } from "./Components/Header";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Home } from "./pages/Home";
+import { Artists } from "./pages/Artists";
+import { Songs } from "./pages/Songs";
+import { Artist } from "./pages/Artist";
+import { Song } from "./pages/Song";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <BandsSection />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/allArtists" element={<Artists />} />
+        <Route path="/artist/:id" element={<Artist />} />
+        <Route path="/allSongs" element={<Songs />} />
+        <Route path="/song/:id" element={<Song />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
