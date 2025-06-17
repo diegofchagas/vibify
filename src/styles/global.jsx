b4b9a735b5ee720e6da40b4f7fda6d9f255d5639
Diego Chagas <diegofchagas@gmail.com>
2025-06-17 11:06:00 -0300

    add class ao css global do tippy para estlizacao do mesmo

--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -31,6 +31,17 @@ img{
 #root{
 height:100svh;
 }
+
+.tippy-box[data-theme~='custom-play'] {
+    background-color: #202020;
+    color: #fff;
+    font-weight: bold;
+    font-size: 0.8rem;
+    padding: 5px 8px;
+    border-radius: 4px;
+    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.5);
+  }
+
 /* *,::before,::after{
     margin:0;
     padding:0;
